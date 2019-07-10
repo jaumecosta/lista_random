24965353ba9b43aaabe46e906b48c5be435eef59
jaumecosta <jaumecostagomez@gmail.com>
2019-07-10 14:53:04 +0200

    LISTA INFORMATICA CON LOS DATOS

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
-$persona 
+$nombres = [
+    'toni' => ' HTML5/CSS3',
+    'jorge' => 'Javascript',
+    'julia' => 'PHP', 
+    'jose' => 'Json/XML',
+    'mateo' => 'Python', 
+    'jaume' => 'Java',
+    'petro' => 'Sass/Less',
+     'alejandro' => 'Laravel',
+     'fran' => ' HTML5/CSS3',
+     'adrian' => 'Javascript', 
+     'lolo'  => 'PHP', 
+     'cristian' => 'Json/XML',
+     'jordi' => 'Python',
+      'david'  => 'Java',
+       'luis'  => 'Python', 
+       'alex' => 'Laravel',
+    ];
 
 
 ?>
